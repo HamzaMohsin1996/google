dbb7b107c41a784612465a406b2c8f718762ba34
Hamzawarraic <40853399+Hamzawarraic@users.noreply.github.com>
2022-04-25 23:40:16 +0500

    added(ui):modal styling

--- src/Components/ExpenseCalculate/ExpenseItem.js
+++ src/Components/ExpenseCalculate/ExpenseItem.js
@@ -1,15 +1,13 @@
 import {React, useState} from 'react';
-import { Typography,Button } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Calender from '../Calender';
-import Modal from '../Modal/index';
+
 
 const  ExpenseItem = (props) =>{
   // const [title,setTitle] = useState(props.title);
 
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
     return (  
       <div>
         <Box sx={{padding: '20px',background: '#ebebeb',borderRadius: '10px',margin:'10px 0',display:'flex',alignItems:'center',justifyContent:'flex-start'}}>
@@ -18,11 +16,11 @@ const  ExpenseItem = (props) =>{
           <Typography variant="h5">{props.title}</Typography> 
           <Box sx={{display:'flex',alignItems:'center',justifyContent:'flex-start'}}>
           <Typography variant="h6" sx={{mr:3}}>${props.ammount}</Typography> 
-          <Button variant="contained" onClick={handleOpen}>Add Expense</Button>
+          
           </Box>
           </Box>
         </Box>
-        <Modal handleOpen={open} handleClose={handleClose}/>
+        
       </div>
     );
   }
